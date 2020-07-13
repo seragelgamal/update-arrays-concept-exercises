ddf12061e9d21d916376c2ddc1bcb5e176fea7bb
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-13 13:04:09 -0600

    commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// q1
+// a
+array1[0] = 'magenta';
+
+// b
+array1[2] += 700;
+
+// c
+array1[array1.length - 1] = 'the end';
+
+// d
+array1.push(70);
+
+// e
+array1.unshift(500);
+
+// f
+array1.splice(5, 0, 80);
